2de3a36740df0f98a095a296538630698fb64567
Gayatri Satish Suryawanshi <89183954+Gayatri3108@users.noreply.github.com>
2023-02-27 18:41:55 +0530

    second commit

--- dayfourteen/src/org/tnsif/client/Client.java
+++ dayfourteen/src/org/tnsif/client/Client.java
@@ -1,117 +1,115 @@
 package org.tnsif.client;
-//import java.util.List;
+
+import java.util.List;
 import java.util.Scanner;
 
 import org.tnsif.entity.Employee;
 import org.tnsif.service.EmployeeService;
-//import org.tnsif.service.EmployeeServiceImpl;
+import org.tnsif.service.EmployeeServeceImpl;
 
 //driver class
 public class Client {
 
 	public static void main(String[] args) {
 		int choice;
-		//EmployeeService service=new EmployeeServiceImpl();
-		Scanner sc=new Scanner(System.in);
-		do
-		{
-			System.out.println("Enter the choice: ");
-			System.out.println("1.Insert "+"2.update"+"3.delete"+"4.retriveall"+
-			"5.retreive"+ "\n-1: exit");
-			choice=sc.nextInt();
-			switch(choice)
+		EmployeeService service=new EmployeeServeceImpl();
+		try (Scanner sc = new Scanner(System.in)) {
+			do
 			{
-			case 1:
-			{
-				System.out.println("Enter the details of employee");
-				Employee employee=new Employee(sc.nextInt(),sc.next(),sc.next(),sc.next());
-				if(service.addEmployee(employee))
+				System.out.println("Enter the choice: ");
+				System.out.println("1.Insert "+"2.update"+"3.delete"+"4.retriveall"+
+				"5.retreive"+ "\n-1: exit");
+				choice=sc.nextInt();
+				switch(choice)
 				{
-					System.out.println("added successfully");
+				case 1:
+				{
+					System.out.println("Enter the details of employee");
+					Employee employee=new Employee(sc.nextInt(),sc.next(),sc.next(),sc.next());
+					if(service.addEmployee(employee))
+					{
+						System.out.println("added successfully");
+					}
+					else
+					{
+						System.out.println("details not found");
+					}
 				}
-				else
+				break;
+				case 2:
 				{
-					System.out.println("details not found");
+					System.out.println("enter the userid whose details want to update");
+					Employee employee=new Employee();
+					employee.setUserid(sc.nextInt());
+					System.out.println("enter the updated details: ");
+					//sc.nextLine();
+					employee.setPassword(sc.next());
+					Employee emp=service.updateEmployee(employee);
+					
+					if(emp!=null)
+					{
+						System.out.println(emp);
+					}
+					else
+					{
+						System.out.println("not updated");
+					}
 				}
-			}
-			break;
-			case 2:
-			{
-				System.out.println("enter the userid whose details want to update");
-				Employee employee=new Employee();
-				employee.setUserid(sc.nextInt());
-				System.out.println("enter the updated details: ");
-				//sc.nextLine();
-				employee.setPassword(sc.next());
-				Employee emp=service.updateEmployee(employee);
-				
-				if(emp!=null)
+				break;
+				case 3:
 				{
-					System.out.println(emp);
+					System.out.println("Enter the UID of employee");
+					boolean success=service.deleteEmployee(sc.nextInt());
+					if(success)
+					{
+						System.out.println("deleted");
+					}
+					else
+					{
+						System.out.println("data not found");
+					}
 				}
-				else
+				break;
+				case 4:
 				{
-					System.out.println("not updated");
-				}
-			}
-			break;
-			case 3:
-			{
-				System.out.println("Enter the UID of employee");
-				boolean success=service.deleteEmployee(sc.nextInt());
-				if(success)
-				{
-					System.out.println("deleted");
-				}
-				else
-				{
-					System.out.println("data not found");
-				}
-			}
-			break;
-			case 4:
-			{
-				List<Employee>emp=service.getAll();
-				//System.out.println(emp);
-				for(Employee e1:emp)
-					System.out.println(e1);
-				
-			}
-			break;
-			case 5:
-			{
-				System.out.println("Enter the id that u want to show: ");
-				Employee employee;
-				try
-				{
-					employee=service.getEmployee(sc.nextInt());
+					List<Employee>emp=service.getAll();
+					//System.out.println(emp);
+					for(Employee e1:emp)
+						System.out.println(e1);
 					
 				}
-				catch(Exception e)
+				break;
+				case 5:
 				{
-					employee=null;
-					System.out.println(e);
+					System.out.println("Enter the id that u want to show: ");
+					Employee employee;
+					try
+					{
+						employee=service.getEmployee(sc.nextInt());
+						
+					}
+					catch(Exception e)
+					{
+						employee=null;
+						System.out.println(e);
+					}
+					if(employee!=null)
+					{
+						System.out.println(employee);
+					}
+					else
+					{
+						System.out.println("data not found");
+					}
 				}
-				if(employee!=null)
-				{
-					System.out.println(employee);
+				break;
+				case -1:
+					System.out.println("thank you");
 				}
-				else
-				{
-					System.out.println("data not found");
-				}
-			}
-			break;
-			case -1:
-				System.out.println("thank you");
-			}
-			}while(choice>0);
+				}while(choice>0);
+		}
 			
 			
 	}
 			
-			
-		
-	
-
 }
